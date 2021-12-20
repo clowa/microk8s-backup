93b32608c56ad4db5927f8c054b4108a2a4cfe96
Cedric Ahlers <cedric.ahlers@posteo.de>
2021-12-20 18:56:53 +0100

    update parseing to use build in parser of logfmt library

--- app/src/lib/logger.ts
+++ app/src/lib/logger.ts
@@ -10,31 +10,20 @@ export enum logLevel {
 }
 
 export function log(log: { level: logLevel; msg: string }, additional?: Record<string, unknown>) {
-  logfmt.log({ ...{ time: `${new Date().toISOString()}` }, ...log, ...additional });
+  if (log.level == logLevel.Panic || log.level == logLevel.Fatal) {
+    logfmt.log({ ...{ time: `${new Date().toISOString()}` }, ...log, ...additional }, process.stderr);
+  } else {
+    logfmt.log({ ...{ time: `${new Date().toISOString()}` }, ...log, ...additional });
+  }
 }
 
 export function parseGoMigratorLog(rl: Interface) {
   rl.on("line", (data) => {
     try {
-      const message = parser(data);
+      const message = logfmt.parse(data);
       logfmt.log(message);
     } catch (err) {
       log({ level: logLevel.Warning, msg: "Cant parese log message: " + err });
     }
   });
-}
-
-function parser(str: string): Record<string, unknown> {
-  const pattern = new RegExp(
-    'time="?(?<time>[\\d]{4}-[\\d]{2}-[\\d]{2}T[\\d]{2}:[\\d]{2}:[\\d]{2}(\\.[\\d]{2,3})?Z)"?\\slevel=(?<level>\\w*)\\smsg="?(?<msg>.*?)(\\n)?"'
-  );
-
-  const result = pattern.exec(str);
-
-  if (result != null) {
-    // console.log(result.groups);
-    return result.groups!;
-  }
-
-  return {};
 }
